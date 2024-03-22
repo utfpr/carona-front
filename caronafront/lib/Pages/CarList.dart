c14cbec50b2d0cb4f9171cafe08e2f0ec077b18a
Guilherme Almeida Lopes <guiguigui098@gmail.br>
2024-03-21 22:47:17 -0300

    fix: format CarRegisterPage

--- caronafront/lib/Pages/CarList.dart
+++ caronafront/lib/Pages/CarList.dart
@@ -7,10 +7,9 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class CarList extends StatefulWidget {
-
   CarList({super.key, required this.user});
   final User user;
-  late Future<List<Car>?>car;
+  late Future<List<Car>?> car;
   @override
   State<CarList> createState() => _CarListState();
 }
@@ -20,81 +19,94 @@ class _CarListState extends State<CarList> {
   void dispose() {
     super.dispose();
   }
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    widget.car=APIservicosCar.getallcar(widget.user.id);
+    widget.car = APIservicosCar.getallcar(widget.user.id);
   }
-  Column mgsnogetall(String msg){
+
+  Column mgsnogetall(String msg) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Text(msg,style: TextStyle(
-          color: Colors.white.withOpacity(0.1),
-          fontSize: 12
-        ),),
+        Text(
+          msg,
+          style: TextStyle(color: Colors.white.withOpacity(0.1), fontSize: 12),
+        ),
         SizedBox(
-          child: Icon(Icons.car_crash,size: 100,
-          color:Colors.white.withOpacity(0.1),),
+          child: Icon(
+            Icons.car_crash,
+            size: 100,
+            color: Colors.white.withOpacity(0.1),
+          ),
           width: 100,
           height: 100,
         )
       ],
     );
   }
-  
-  AppBar buildappbar(BuildContext context,{required double heightbar,required Color color, 
-  required double radiuscircle,required double heightsizebox}){
+
+  AppBar buildappbar(BuildContext context,
+      {required double heightbar,
+      required Color color,
+      required double radiuscircle,
+      required double heightsizebox}) {
     return AppBar(
-    actions: [
-      Padding(padding:const EdgeInsetsDirectional.all(15) ,
-      child:CircleAvatar(
-      radius: radiuscircle*MediaQuery.of(context).size.height,backgroundColor:Color.fromARGB(221, 51, 39, 153) ,),)],
-      toolbarHeight: heightbar*MediaQuery.of(context).size.height,
-    backgroundColor: color,
-    title: const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center, 
-      mainAxisSize: MainAxisSize.min,
-      children: [
-      Text("Olá,"),
-      Text("Caronas rápido e fácil")
-    ]),);
+      actions: [
+        Padding(
+          padding: const EdgeInsetsDirectional.all(15),
+          child: CircleAvatar(
+            radius: radiuscircle * MediaQuery.of(context).size.height,
+            backgroundColor: Color.fromARGB(221, 51, 39, 153),
+          ),
+        )
+      ],
+      toolbarHeight: heightbar * MediaQuery.of(context).size.height,
+      backgroundColor: color,
+      title: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [Text("Olá,"), Text("Caronas rápido e fácil")]),
+    );
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    final msgnotfoundcar=Center(child: mgsnogetall("Nao encontrado carros"),);
+    final msgnotfoundcar = Center(
+      child: mgsnogetall("Nao encontrado carros"),
+    );
     return Scaffold(
-    appBar:buildappbar(context,heightbar: 0.2,
-    radiuscircle: 0.05,heightsizebox: 0.01,
-    color: Colors.black12,
-    ),
-    body: FutureBuilder<List<Car>?>(
-      future: widget.car,
-      builder: (ctx,list){
-        if (list.hasData) {
-          return ListView.builder(
-          itemCount: list.data!.length,
-          itemBuilder: (ctx,index){
-            return CarTitle(car: list.data!.elementAt(index));
-          }
-          );
-        }
-        return msgnotfoundcar;
-      }
+      appBar: buildappbar(
+        context,
+        heightbar: 0.2,
+        radiuscircle: 0.05,
+        heightsizebox: 0.01,
+        color: Colors.black12,
       ),
-    floatingActionButton:FloatingActionButton(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(180)
+      body: FutureBuilder<List<Car>?>(
+          future: widget.car,
+          builder: (ctx, list) {
+            if (list.hasData) {
+              return ListView.builder(
+                  itemCount: list.data!.length,
+                  itemBuilder: (ctx, index) {
+                    return CarTitle(car: list.data!.elementAt(index));
+                  });
+            }
+            return msgnotfoundcar;
+          }),
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(180)),
+        backgroundColor: Colors.yellow,
+        hoverColor: Colors.yellow,
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => CarRegisterPage(user: widget.user)));
+        },
+        child: Icon(Icons.add),
       ),
-      backgroundColor: Colors.yellow,
-      hoverColor: Colors.yellow,
-      onPressed:(){
-      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>CarRegisterPage(user:widget.user)));
-    },
-    child: Icon(Icons.add),
-    ),
     );
   }
 }
