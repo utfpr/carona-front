a931a3e44acf26ddb7602e27f8045ec48c7ea621
Guilherme Almeida Lopes <guiguigui098@gmail.br>
2024-03-21 22:53:12 -0300

    feat: format APiservicos

--- caronafront/lib/servicos/APIservicosCar.dart
+++ caronafront/lib/servicos/APIservicosCar.dart
@@ -2,101 +2,101 @@ import 'dart:convert';
 import 'package:caronafront/model/Carmodel.dart';
 import 'package:http/http.dart' as http;
 
-class APIservicosCar{
-
-  static Future<int> createcar(String plate,String description,String user_id)async{
-    final response=await 
-    http.post(Uri.parse("http://localhost:3333/car"),
-    headers: <String,String>{
-      'Content-Type': 'application/json; charset=UTF-8', 
-    },body: jsonEncode({
-    "plate": plate,
-    "description": description,
-    "userId": user_id,
-    }),
+class APIservicosCar {
+  static Future<int> createcar(
+      String plate, String description, String user_id) async {
+    final response = await http.post(
+      Uri.parse("http://localhost:3333/car"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        "plate": plate,
+        "description": description,
+        "userId": user_id,
+      }),
     );
-    if (response.statusCode==201) {
+    if (response.statusCode == 201) {
       return -1;
     } else {
       return 0;
     }
   }
 
-  static Future<int> updatecar(String carid,String platenew,
-  String userid,String descriptionnew)async{
-  final response=await http.put(
-    Uri.parse("http://localhost:3333/car"+carid),
-    headers: <String,String>{
-      'Content-Type': 'application/json; charset=UTF-8'
-    },body: jsonEncode(
-    {
-    "plate": platenew,
-    "description": descriptionnew,
-    "userId": userid ,
-    })
-  );
-    if (response.statusCode==201) {
+  static Future<int> updatecar(String carid, String platenew, String userid,
+      String descriptionnew) async {
+    final response = await http.put(
+        Uri.parse("http://localhost:3333/car" + carid),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode({
+          "plate": platenew,
+          "description": descriptionnew,
+          "userId": userid,
+        }));
+    if (response.statusCode == 201) {
       return 0;
     } else {
       return -1;
     }
   }
 
-  static Future<int>deletecar(String carid) async{
-    final response=await http.delete(Uri.parse("http://localhost:3333/car"+carid,),
-    headers: <String,String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    }
-    );
-    if (response.statusCode==200) {
-      return 0;      
+  static Future<int> deletecar(String carid) async {
+    final response = await http.delete(
+        Uri.parse(
+          "http://localhost:3333/car" + carid,
+        ),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+    if (response.statusCode == 200) {
+      return 0;
     } else {
       return -1;
     }
   }
 
-  static Future<Car?>fectchcar(String id,)async{
-    final response=await http.get(
-      Uri.parse("http://localhost:3333/car/"+id),
-      headers: <String,String>{
+  static Future<Car?> fectchcar(
+    String id,
+  ) async {
+    final response = await http.get(
+      Uri.parse("http://localhost:3333/car/" + id),
+      headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8'
       },
     );
-    if (response.statusCode==200) {
-      final json_car=jsonDecode(response.body) as Map<String,dynamic>;
-      return Car(json_car["id"] as String, json_car["plate"] as String, 
-      json_car["description"] as String,json_car["id"] as String, 
-      createdAt: json_car["createdAt"] as DateTime? , 
-      updateAt: json_car["updateAt"] as DateTime?);
+    if (response.statusCode == 200) {
+      final json_car = jsonDecode(response.body) as Map<String, dynamic>;
+      return Car(json_car["id"] as String, json_car["plate"] as String,
+          json_car["description"] as String, json_car["id"] as String,
+          createdAt: json_car["createdAt"] as DateTime?,
+          updateAt: json_car["updateAt"] as DateTime?);
     } else {
       return null;
     }
   }
 
-  static Future<List<Car>?> getallcar(String id)async{
-    final response=await http.get(
-    Uri.parse("http://localhost:3333/car/user/"+id),
-    headers: <String,String>{
-      'Content-Type': 'application/json; charset=UTF-8' 
+  static Future<List<Car>?> getallcar(String id) async {
+    final response = await http.get(
+        Uri.parse("http://localhost:3333/car/user/" + id),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        });
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      print(response.body);
+      print(response.statusCode);
+      List<Car> lista = [];
+      for (var element in json) {
+        lista.add(Car(element["id"] as String, element["plate"] as String,
+            element["description"] as String, element["userId"] as String,
+            createdAt: null, updateAt: null));
+      }
+      return lista;
     }
-  );
-  if (response.statusCode==200) {
-    final json=jsonDecode(response.body);
-    print(response.body);
-    print(response.statusCode);
-    List<Car> lista=[];
-    for (var element in json) {
-      lista.add(Car(element["id"] as String , 
-      element["plate"] as String, element["description"] as String, 
-      element["userId"] as String, createdAt:null, 
-      updateAt:null)
-      );
-    }
-    return lista;
-  } 
-  return null;
-}
-  
+    return null;
+  }
 }
 
   
